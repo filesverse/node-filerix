5b83b395a78433454024ad803ce77da770d98c2f
KingMaj0r <abdurrahman@barakat.email>
2025-02-23 18:21:23 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-set -e
-
-PREFIX="/usr/local"
-if [[ "$1" == "--prefix="* ]]; then
-  PREFIX="${1#--prefix=}"
-fi
-
 echo "Downloading filerix vcpkg port..."
 git clone --recurse-submodules https://github.com/filesverse/vcpkg-port.git || { echo "Failed to download filerix vcpkg port"; exit 1; }
 
@@ -20,11 +13,11 @@ echo "Installing dependencies with vcpkg..."
 ./vcpkg/vcpkg --feature-flags=manifests install || { echo "Failed to install dependencies"; exit 1; }
 
 echo "Generating build files with CMake..."
-cmake -B build -S . \
-  -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-  -DCMAKE_INSTALL_LIBDIR="$PREFIX/lib64" || { echo "Failed to generate CMake build files"; exit 1; }
+cd build 
+cmake .. || { echo "Failed to generate CMake build files"; exit 1; }
 
 echo "Building the project..."
-cmake --build build || { echo "Build failed"; exit 1; }
+cmake --build . --parallel || { echo "Build failed"; exit 1; }
+cd ..
 
 echo "Installation and build complete!"

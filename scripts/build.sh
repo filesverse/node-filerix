7439321564ec67a1f5852d1af805924d862fd211
KingMaj0r <abdurrahman@barakat.email>
2025-02-23 17:28:00 +0700

    Added build.sh. Update install.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,16 +5,11 @@ echo "Using temporary directory: $TMP_DIR"
 
 cd "$TMP_DIR" || { echo "Failed to enter temp directory"; exit 1; }
 
-echo "Cloning the repository with submodules..."
-git clone --recurse-submodules https://github.com/filesverse/node-filerix.git || { echo "Failed to clone repository"; exit 1; }
-
 echo "Downloading filerix vcpkg port..."
 git clone --recurse-submodules https://github.com/filesverse/vcpkg-port.git || { echo "Failed to download filerix vcpkg port"; exit 1; }
 
 echo "Copying filerix vcpkg port..."
-mv ./vcpkg-port ./node-filerix/vcpkg/ports/filerix || { echo "Failed to copy filerix vcpkg port"; exit 1; }
-
-cd node-filerix || { echo "Failed to enter the project directory"; exit 1; }
+mv $TMP_DIR/vcpkg-port ./vcpkg/ports/filerix || { echo "Failed to copy filerix vcpkg port"; exit 1; }
 
 echo "Bootstrapping vcpkg..."
 ./vcpkg/bootstrap-vcpkg.sh || { echo "Failed to bootstrap vcpkg"; exit 1; }
@@ -27,11 +22,6 @@ cmake -B build -S . || { echo "Failed to generate cmake build files"; exit 1; }
 
 echo "Building the project..."
 cmake --build build || { echo "Build failed"; exit 1; }
-
-echo "Installing the built project..."
-cd build && sudo make install || { echo "Installation failed"; exit 1; }
-
-cd "$HOME" || { echo "Failed to return to home directory"; exit 1; }
 
 echo "Removing temporary folder: $TMP_DIR"
 rm -rf "$TMP_DIR" || { echo "Removing temp folder failed"; exit 1; }

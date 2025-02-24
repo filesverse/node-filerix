0cb6268525a8bdfd572b8146f103f788882027a8
KingMaj0r <abdurrahman@barakat.email>
2025-02-24 11:17:21 +0700

    Update CMakeLists.txt and Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,7 @@ echo "Bootstrapping vcpkg..."
 ./vcpkg/bootstrap-vcpkg.sh || { echo "Failed to bootstrap vcpkg"; exit 1; }
 
 echo "Installing dependencies with vcpkg..."
-./vcpkg/vcpkg --feature-flags=manifests install || { echo "Failed to install dependencies"; exit 1; }
+./vcpkg/vcpkg --feature-flags=manifests install --triplet x64-linux-release || { echo "Failed to install dependencies"; exit 1; }
 
 echo "Generating build files with CMake..."
 mkdir build && cd build
@@ -18,5 +18,9 @@ cmake .. || { echo "Failed to generate CMake build files"; exit 1; }
 
 echo "Building the project..."
 cmake --build . --parallel || { echo "Build failed"; exit 1; }
+cd ..
+
+echo "Removing RPATH from filerix.node..."
+chrpath --delete ./build/filerix.node || echo "No RPATH to remove"
 
 echo "Installation and build complete!"
